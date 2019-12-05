374f035f32344e2bc9f8d8d1052eb6dbf01d4984
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-12-05 18:10:54 +0100

    Updates

---
+++ precache-manifest.73c4abd9ade5ac84ea306578a47faaff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2648125dfcec5ae452f6ebfb02fbd7",
+    "url": "/spacex-graphql/index.html"
+  },
+  {
+    "revision": "e8c6bc2b1debb1be2b96",
+    "url": "/spacex-graphql/static/css/main.c9fd153a.chunk.css"
+  },
+  {
+    "revision": "2e88dc5f1e47574c37e5",
+    "url": "/spacex-graphql/static/js/2.ae5a90eb.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/spacex-graphql/static/js/2.ae5a90eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8c6bc2b1debb1be2b96",
+    "url": "/spacex-graphql/static/js/main.d9f4ec42.chunk.js"
+  },
+  {
+    "revision": "c186d2e1ca2ad1886980",
+    "url": "/spacex-graphql/static/js/runtime-main.6885bbff.js"
+  }
+]);
